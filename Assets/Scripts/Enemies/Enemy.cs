6f6d9baee7aa8f7b48c34a704a91cf69544de153
Tri Quyet <nguyentriquyet010190@gmail.com>
2025-07-25 23:57:21 +0700

    aaaaa

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace FGUIStarter.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+            
+                GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+                if (spawnPoint != null)
+                {
+                    collision.gameObject.transform.position = spawnPoint.transform.position;
+                }
+                else
+                {
+                    // Default spawn position if no spawn point is found
+                    collision.gameObject.transform.position = new Vector3(-25f, 8f, 0f);
+                }
+            }
+        }
+    }
+}
